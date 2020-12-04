93de49ef8413c5cabef83f9d47e002c93966a5ae
Shubham14019 <54889779+Shubham14019@users.noreply.github.com>
2020-12-04 18:47:48 +0530

    file deleted

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/hello-world'),
+      dir: require('path').join(__dirname, './coverage/my-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
